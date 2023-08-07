dbda92c8d0ced4ecaa1d53fa6fd91b4487c630d6
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-07 12:02:20 +0000

    writing the function for the imagepicker

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
+import React, { useState, useEffect } from 'react';
+import {
+    View, Text, TouchableOpacity,
+    ScrollView, StatusBar, Image,Alert,ActivityIndicator
+} from 'react-native';
+import { TailwindProvider } from "tailwindcss-react-native";
+import * as ImagePicker from 'expo-image-picker';
+import {
+    Ionicons} from "@expo/vector-icons";
 const App = () => {
+  const [CardImage, SetCardImage] = useState(null);
+
+  const handleImagePicker = async (imageType) => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+    });
+
+    if (!result.canceled) {
+      SetCardImage(result.assets[0].uri); 
+    }
+};
+
   return (
-    <View>
-      <Text>App</Text>
+    <TailwindProvider>
+      <View>
+     
     </View>
+    </TailwindProvider>
+   
   )
 }
 
